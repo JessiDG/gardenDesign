a34d3f3e9873031cb39c36aacb6f4c45714c4241
Jessica Dickinson Goodman <jessica.dickinson.goodman@gmail.com>
2020-08-11 11:56:51 -0700

    Updating the Plant class

---
+++ src/plant.java
@@ -0,0 +1,230 @@
+public class plant {
+
+    int popularityRanking = 0;
+    String commonName = "";
+    String currentBotanicalName	= "";
+    String plantType = "";
+//    String height = "";
+//    String width = "";
+    int heightLowestFt = 0;
+    int heightHighestFt = 0;
+    int heightLowestFMetric = 0;
+    int heightHighestFMetric = 0;
+    int widthLowestFt = 0;
+    int widthHighestFt = 0;
+    int widthLowestFMetric = 0;
+    int widthHighestFMetric = 0;
+    String growthRate = "";
+    String deciduousVsEvergreen = "";
+    String flowerColor = "";
+    String floweringSeason = "";
+    String sun = "";
+    String soilDrainage = "";
+    String waterRequirement = "";
+    String easeofCare = "";
+    String maxSummerIrrigation = "";
+    String commonUses = "";
+    String availabilityInNurseries = "";
+    String plantUrl = "";
+    String QRCodes = ""; // note: change number to change image size in pixels
+
+    public void setPopularityRanking(int popularityRanking) {
+        this.popularityRanking = popularityRanking;
+    }
+
+    public void setCommonName(String commonName) {
+        this.commonName = commonName;
+    }
+
+    public void setCurrentBotanicalName(String currentBotanicalName) {
+        this.currentBotanicalName = currentBotanicalName;
+    }
+
+    public void setHeightHighestFt(int heightHighestFt) {
+        this.heightHighestFt = heightHighestFt;
+    }
+
+    public void setHeightHighestFMetric(int heightHighestFMetric) {
+        this.heightHighestFMetric = heightHighestFMetric;
+    }
+
+    public void setHeightLowestFMetric(int heightLowestFMetric) {
+        this.heightLowestFMetric = heightLowestFMetric;
+    }
+
+    public void setGrowthRate(String growthRate) {
+        this.growthRate = growthRate;
+    }
+
+    public void setHeightLowestFt(int heightLowestFt) {
+        this.heightLowestFt = heightLowestFt;
+    }
+
+    public void setPlantType(String plantType) {
+        this.plantType = plantType;
+    }
+
+    public void setWidthHighestFMetric(int widthHighestFMetric) {
+        this.widthHighestFMetric = widthHighestFMetric;
+    }
+
+    public void setAvailabilityInNurseries(String availabilityInNurseries) {
+        this.availabilityInNurseries = availabilityInNurseries;
+    }
+
+    public void setCommonUses(String commonUses) {
+        this.commonUses = commonUses;
+    }
+
+    public void setDeciduousVsEvergreen(String deciduousVsEvergreen) {
+        this.deciduousVsEvergreen = deciduousVsEvergreen;
+    }
+
+    public void setFlowerColor(String flowerColor) {
+        this.flowerColor = flowerColor;
+    }
+
+    public void setWidthHighestFt(int widthHighestFt) {
+        this.widthHighestFt = widthHighestFt;
+    }
+
+    public void setEaseofCare(String easeofCare) {
+        this.easeofCare = easeofCare;
+    }
+
+    public void setWidthLowestFMetric(int widthLowestFMetric) {
+        this.widthLowestFMetric = widthLowestFMetric;
+    }
+
+    public void setWidthLowestFt(int widthLowestFt) {
+        this.widthLowestFt = widthLowestFt;
+    }
+
+    public void setFloweringSeason(String floweringSeason) {
+        this.floweringSeason = floweringSeason;
+    }
+
+    public void setMaxSummerIrrigation(String maxSummerIrrigation) {
+        this.maxSummerIrrigation = maxSummerIrrigation;
+    }
+
+    public void setPlantUrl(String plantUrl) {
+        this.plantUrl = plantUrl;
+    }
+
+    public void setQRCodes(String QRCodes) {
+        this.QRCodes = QRCodes;
+    }
+
+    public void setSoilDrainage(String soilDrainage) {
+        this.soilDrainage = soilDrainage;
+    }
+
+    public void setSun(String sun) {
+        this.sun = sun;
+    }
+
+    public void setWaterRequirement(String waterRequirement) {
+        this.waterRequirement = waterRequirement;
+    }
+
+    public String getCommonName() {
+        return commonName;
+    }
+
+    public int getHeightHighestFMetric() {
+        return heightHighestFMetric;
+    }
+
+    public int getHeightHighestFt() {
+        return heightHighestFt;
+    }
+
+    public int getHeightLowestFt() {
+        return heightLowestFt;
+    }
+
+    public int getHeightLowestFMetric() {
+        return heightLowestFMetric;
+    }
+
+    public int getPopularityRanking() {
+        return popularityRanking;
+    }
+
+    public int getWidthHighestFMetric() {
+        return widthHighestFMetric;
+    }
+
+    public String getCurrentBotanicalName() {
+        return currentBotanicalName;
+    }
+
+    public String getPlantType() {
+        return plantType;
+    }
+
+    public int getWidthHighestFt() {
+        return widthHighestFt;
+    }
+
+    public int getWidthLowestFMetric() {
+        return widthLowestFMetric;
+    }
+
+    public int getWidthLowestFt() {
+        return widthLowestFt;
+    }
+
+    public String getAvailabilityInNurseries() {
+        return availabilityInNurseries;
+    }
+
+    public String getCommonUses() {
+        return commonUses;
+    }
+
+    public String getDeciduousVsEvergreen() {
+        return deciduousVsEvergreen;
+    }
+
+    public String getFlowerColor() {
+        return flowerColor;
+    }
+
+    public String getEaseofCare() {
+        return easeofCare;
+    }
+
+    public String getFloweringSeason() {
+        return floweringSeason;
+    }
+
+    public String getGrowthRate() {
+        return growthRate;
+    }
+
+    public String getMaxSummerIrrigation() {
+        return maxSummerIrrigation;
+    }
+
+    public String getPlantUrl() {
+        return plantUrl;
+    }
+
+    public String getQRCodes() {
+        return QRCodes;
+    }
+
+    public String getSoilDrainage() {
+        return soilDrainage;
+    }
+
+    public String getSun() {
+        return sun;
+    }
+
+    public String getWaterRequirement() {
+        return waterRequirement;
+    }
+}
--- src/readCSVIntoPlant.java
+++ src/readCSVIntoPlant.java
@@ -1,2 +1,17 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class readCSVIntoPlant {
+
+    public static String[][] readCSVIntoArray(String pathToCSV) throws IOException {
+        BufferedReader csvReader = new BufferedReader(new FileReader(pathToCSV));
+        String row;
+        while ((row = csvReader.readLine()) != null) {
+            String[] data = row.split(",");
+            // do something with the data
+        }
+        csvReader.close();
+    }
 }
